b071409a4f385460212ae673d33b3bbf5b8a2b22
svukelic <sanja.vukelic@avisto-eastern.com>
2022-12-13 11:09:49 +0100

    configuracioni fajl

---
+++ config.py
@@ -0,0 +1,30 @@
+from configparser import ConfigParser
+
+config = ConfigParser()
+
+config['GITLAB'] = {
+    "hostGitlab": 'http://192.168.10.200:8083/',
+    "passwordGitlab": "glpat-xAyPHPx7pJfC2knxNnHx"
+}
+
+config['JENKINS'] = {
+    "hostJenkins": "http://192.168.10.200:8082/",
+    "usernameJenkins": "svukelic",
+    "passwordJenkins": "114cf2d71d18de7dd85b1ca7a2ecee0067",
+    "job": "maven2"
+
+}
+
+config['JIRA'] = {
+    "hostJira": 'http://192.168.10.200:8086/',
+    "API_token_jira": 'OTUwNjA0MDUyNjgxOl7cmORGLvsRpUyI2S7Cj4e5DcJE',
+    "issue_key": "TES-1"
+}
+
+config['HOST'] = {
+    "host": '192.168.10.200',
+    "port": '3538'
+}
+
+with open("parametersweb.ini", "w") as f:
+    config.write(f)
--- webpython.py
+++ webpython.py
@@ -5,6 +5,10 @@ import gitlab
 import jenkins
 import requests
 from jira import JIRA
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read("parametersweb.ini")
 
 
 app = Flask(__name__)
@@ -12,24 +16,30 @@ app = Flask(__name__)
 @app.route('/gitlab',methods=['POST'])
 def foo():
    data = json.loads(request.data)
-   print("\nGITLAB Project")
+   print("GITLAB Project")
    print (f"Project: {data['project']}\n"
           f"Event: {data['object_kind']}\n\n")
 
     #push->jenkins build job
    if data['object_kind'] == 'push':
-      host = "http://192.168.10.200:8082/"
-      usernameJenkins = "svukelic"
-      passwordJenkins = "114cf2d71d18de7dd85b1ca7a2ecee0067"
-      server = jenkins.Jenkins(host, username=usernameJenkins, password=passwordJenkins)
-      server.build_job('maven2')
+      config_data = config["JENKINS"]
+      hostJenkins = config_data['hostjenkins']
+      usernameJenkins = config_data['usernamejenkins']
+      passwordJenkins = config_data['passwordjenkins']
+      server = jenkins.Jenkins(hostJenkins, username=usernameJenkins, password=passwordJenkins)
+      #jobs = server.get_jobs()
+      #print("JENKINS JOBS")
+      #print(jobs)
+      job = config_data['job']
+      server.build_job(job)
       print("\n")
 
       #new branch->mergereq
       if data['before'] == '0000000000000000000000000000000000000000':
-         url = 'http://192.168.10.200:8083/'
-         passwordGitlab = "glpat-xAyPHPx7pJfC2knxNnHx"
-         gl = gitlab.Gitlab(url=url, private_token=passwordGitlab)
+         config_data1 = config["GITLAB"]
+         hostGitlab = config_data1['hostgitlab']
+         passwordGitlab = config_data1['passwordgitlab']
+         gl = gitlab.Gitlab(url=hostGitlab, private_token=passwordGitlab)
          dataproj = data['project']
          nameproj = dataproj['name']
          idproj = dataproj["id"]
@@ -45,8 +55,12 @@ def foo():
    #mergereq->issue
    elif data['object_kind'] == 'merge_request':
       print("\n")
-      API_token_jira = 'OTUwNjA0MDUyNjgxOl7cmORGLvsRpUyI2S7Cj4e5DcJE'
-      jira = JIRA('http://192.168.10.200:8086/', token_auth=(API_token_jira))
+      config_data2 = config["JIRA"]
+      hostJira = config_data2['hostjira']
+      API_token_jira = config_data2['api_token_jira']
+      jira = JIRA(hostJira, token_auth=(API_token_jira))
+      #projects = jira.projects()
+      #print(projects)
       new_issue = jira.create_issue(project='TES', summary='New issue from jira-python',
                                     description='Look into this one', issuetype={'name': 'Task'})
       print("Jira issue - CREATED")
@@ -60,13 +74,18 @@ def foo1():
     print(data['result'])
 
     #jenkins build-> update issue
-    API_token_jira = 'OTUwNjA0MDUyNjgxOl7cmORGLvsRpUyI2S7Cj4e5DcJE'
-    jira = JIRA('http://192.168.10.200:8086/', token_auth=(API_token_jira))
-    issue = jira.issue("TES-1")
+    config_data3 = config["JIRA"]
+    hostJira = config_data3['hostjira']
+    API_token_jira = config_data3['api_token_jira']
+    jira = JIRA(hostJira , token_auth=(API_token_jira))
+    issue_key = config_data3['issue_key']
+    issue = jira.issue(issue_key)
     jira.add_comment(issue, data['result'])
     return "OK"
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.10.200', port='3538')
-
+    config_data4 = config["HOST"]
+    host = config_data4['host']
+    port = config_data4['port']
+    app.run(host= host, port= port)
